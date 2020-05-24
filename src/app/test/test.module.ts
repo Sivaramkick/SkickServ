7ea0897a78dba695f29ed27ac35632bf2d0a1576
sivarama krishna <sivaramakc@pllcsoftwaresolutions.com>
2020-05-24 19:57:08 +0530

    Test Component Commit

---
+++ src/app/test/test.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { TestComComponent } from './test-com/test-com.component';
+
+
+
+@NgModule({
+  declarations: [TestComComponent],
+  imports: [
+    CommonModule
+  ]
+})
+export class TestModule { }
